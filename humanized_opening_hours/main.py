rezemika/humanized_opening_hours

commit ce5d7aedcdcb3d929d3ac2721c8491f38ba1b105

Shortens the code by removing two useless methods

humanized_opening_hours/main.py
@@ -466,7 +466,6 @@ class HOHRenderer:
         if not locale_name:
             locale_name = "en"
         self.set_locale(locale_name)
-        return
     
     @staticmethod
     def available_locales():
@@ -490,8 +489,8 @@ class HOHRenderer:
         ----------
         locale_name : str
             The locale name. E.g. "en".
-            See HOHRenderer.AVAILABLE_LOCALES to get a tuple of
-            available locales.
+            See HOHRenderer.available_locales() (static method) to get
+            a list of available locales.
         
         Returns
         -------
@@ -526,38 +525,10 @@ class HOHRenderer:
         days = []
         months = []
         for i in range(7):
-            days.append(self.get_locale_day(i))
+            days.append(self.babel_locale.days["format"]["wide"][i])
         for i in range(12):
-            months.append(self.get_locale_month(i))
+            months.append(self.babel_locale.months['format']['wide'][i+1])
         return {"days": days, "months": months}
-    
-    def get_locale_day(self, index):
-        """Returns a day name in the constructor's locale.
-        
-        Parameters
-        ----------
-        int
-            The day's index, between 0 and 6.
-        
-        Returns
-        -------
-        str : The translated day's name.
-        """
-        return self.babel_locale.days["format"]["wide"][index]
-    
-    def get_locale_month(self, index):
-        """Returns a month name in the constructor's locale.
-        
-        Parameters
-        ----------
-        int
-            The month's index, between 0 and 11.
-        
-        Returns
-        -------
-        str : The translated month's name.
-        """
-        return self.babel_locale.months['format']['wide'][index+1]
     
     def _format_date(self, date):
         """Formats a datetime with the appropriate locale.

commit 63a484ad76a57276823f04240c4ed1af6a516ec6

Fixes a regression introduced by ce5d7ae
The commit ce5d7ae removed two methods, but didn't change a part of the code which used one of them.
Consequently, `ohr.plaintext_week_description()` raised an `AttributeError`.

This commit fixes this and adds unit tests to ensure it won't happen again.

humanized_opening_hours/main.py
@@ -693,7 +693,7 @@ class OHRenderer:
                 )
             )
         rendered_periods = self._join_list(rendered_periods)
-        name = self.get_locale_day(day.weekday())
+        name = self.get_human_names()["days"][day.weekday()]
         return RenderableDay(name=name, description=rendered_periods, dt=d.date)
     
     def plaintext_week_description(self, obj=None):

tests.py
@@ -24,6 +24,14 @@ class TestGlobal(unittest.TestCase):
         dt = datetime.datetime(2017, 1, 2, 15, 30, tzinfo=pytz.timezone("Europe/Paris"))
         # Is it open?
         self.assertTrue(oh.is_open(dt))
+        # Rendering.
+        self.assertEqual(
+            oh.render().plaintext_week_description(),
+            "Monday: 09:00 - 19:00\nTuesday: 09:00 - 19:00\n"
+            "Wednesday: 09:00 - 19:00\nThursday: 09:00 - 19:00\n"
+            "Friday: 09:00 - 19:00\nSaturday: 09:00 - 19:00\n"
+            "Sunday: closed"
+        )
     
     def test_2(self):
         field = "Mo,Th 09:00-12:00,13:00-19:00"
@@ -40,6 +48,13 @@ class TestGlobal(unittest.TestCase):
             oh.next_change(dt),
             datetime.datetime(2016, 2, 1, 13, 0, tzinfo=pytz.timezone("UTC"))
         )
+        # Rendering.
+        self.assertEqual(
+            oh.render().plaintext_week_description(),
+            "Monday: 09:00 - 12:00 and 13:00 - 19:00\nTuesday: closed\n"
+            "Wednesday: closed\nThursday: 09:00 - 12:00 and 13:00 - 19:00\n"
+            "Friday: closed\nSaturday: closed\nSunday: closed"
+        )
     
     def test_3(self):
         field = "24/7"
@@ -55,6 +70,14 @@ class TestGlobal(unittest.TestCase):
         self.assertEqual(
             oh.get_day(datetime.date.today()).periods[0].beginning.time(),
             datetime.time.min.replace(tzinfo=pytz.UTC)
+        )
+        # Rendering.
+        self.assertEqual(
+            oh.render().plaintext_week_description(),
+            "Monday: 00:00 - 00:00\nTuesday: 00:00 - 00:00\n"
+            "Wednesday: 00:00 - 00:00\nThursday: 00:00 - 00:00\n"
+            "Friday: 00:00 - 00:00\nSaturday: 00:00 - 00:00\n"
+            "Sunday: 00:00 - 00:00"
         )
     
     def test_4(self):
@@ -91,6 +114,13 @@ class TestGlobal(unittest.TestCase):
                 datetime.date(2018, 1, 11),
                 datetime.time.max.replace(tzinfo=pytz.timezone("UTC"))
             )
+        )
+        # Rendering.
+        self.assertEqual(
+            oh.render().plaintext_week_description(),
+            "Monday: 00:00 - 00:00\nTuesday: 00:00 - 00:00\n"
+            "Wednesday: 00:00 - 00:00\nThursday: 00:00 - 00:00\n"
+            "Friday: 00:00 - 00:00\nSaturday: closed\nSunday: closed"
         )
 
 class TestPatterns(unittest.TestCase):