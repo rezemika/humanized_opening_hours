e4459f4938ffdcafb383a750a8b71f10c39ad398
Michael Marx <michael-marx@protonmail.ch>
2018-06-23 20:29:57 +0200

    Updates version to 1.0.0a1

--- humanized_opening_hours/__init__.py
+++ humanized_opening_hours/__init__.py
@@ -13,7 +13,7 @@ To get started, simply do:
 """
 # flake8: noqa
 
-__version__ = "0.6.2"
+__version__ = "1.0.0a1"
 __appname__ = "osm_humanized_opening_hours"
 __author__ = "rezemika <reze.mika@gmail.com>"
 __licence__ = "AGPLv3"
